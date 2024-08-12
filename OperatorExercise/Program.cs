252c97dba052a24c154a40c4c8d119a7bbf13360
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-08-12 14:05:05 -0500

    Area of Circle Method

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+            
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}.");
+            
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            AreaOfCircle(radius);
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double circleArea = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine($"The area of the circle with radius of {radius} is {circleArea}.");
+            
+            return circleArea;
+        }
+        
     }
 }
